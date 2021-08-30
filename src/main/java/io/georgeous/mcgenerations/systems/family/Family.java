5cfc1f3adf13e70d76da5374c79880d9ad30eaee
stitchinger <g.silitsch@gmail.com>
2021-08-30 18:49:43 +0200

    EXP-bottles and other projectiles should no longer trigger friendly-fire msg when you hit yourself

--- src/main/java/io/georgeous/mcgenerations/systems/family/Family.java
+++ src/main/java/io/georgeous/mcgenerations/systems/family/Family.java
@@ -113,6 +113,7 @@ public class Family {
         boolean notSelf = one != two;
         boolean sameFamily = one.getFamily() == two.getFamily();
         return notSelf && sameFamily;
+
     }
 
     public static boolean inSameFamily(Player one, Player two) {
