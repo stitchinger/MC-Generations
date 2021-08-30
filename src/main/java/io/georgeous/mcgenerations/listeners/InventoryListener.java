3f0b50a0569e57a41854b0cf36cdd7bc9d366cef
BuildTools <unconfigured@null.spigotmc.org>
2021-08-30 16:33:16 +0200

    Added a function to stop babies from opening inventories

--- src/main/java/io/georgeous/mcgenerations/listeners/InventoryListener.java
+++ src/main/java/io/georgeous/mcgenerations/listeners/InventoryListener.java
@@ -10,6 +10,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.inventory.InventoryMoveItemEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
@@ -22,23 +23,7 @@ public class InventoryListener implements Listener {
     }
 
     @EventHandler
-    public void stopBabyItemPickup(EntityPickupItemEvent event) {
-        if (event.getEntity() instanceof Player) {
-            Player player = ((Player) event.getEntity()).getPlayer();
-            PlayerRole role = RoleManager.get(player);
-            if (role == null)
-                return;
-            PhaseManager phaseManager = RoleManager.get(player).pm;
-            if (phaseManager == null)
-                return;
-            if (phaseManager.getCurrentPhase().getName().equalsIgnoreCase("baby")) {
-                event.setCancelled(true);
-            }
-        }
-    }
-
-    @EventHandler
-    public void cancelBabyHandlerDrop(PlayerDropItemEvent event) {
+    public void disableBabyHandlerDrop(PlayerDropItemEvent event) {
         ItemStack item = event.getItemDrop().getItemStack();
         if (ItemManager.isBabyHandler(item)) {
             event.setCancelled(true);
@@ -46,19 +31,37 @@ public class InventoryListener implements Listener {
     }
 
     @EventHandler
-    public void disableBabyInventory(InventoryMoveItemEvent event) {
-        InventoryHolder ih = event.getInitiator().getHolder();
-        if (ih instanceof Player) {
-            Player player = (Player) ih;
-            PlayerRole role = RoleManager.get(player);
-            if (role == null)
-                return;
-            PhaseManager phaseManager = RoleManager.get(player).pm;
-            if (phaseManager == null)
-                return;
-            if (phaseManager.getCurrentPhase().getName().equalsIgnoreCase("baby")) {
+    public void disableBabyItemPickup(EntityPickupItemEvent event) {
+        if (event.getEntity() instanceof Player) {
+            Player player = ((Player) event.getEntity()).getPlayer();
+            if (isABaby(player)) {
                 event.setCancelled(true);
             }
         }
+    }
+
+    @EventHandler
+    public void disableBabyInventoryOpening(InventoryOpenEvent event) {
+        Player player = (Player) event.getPlayer();
+        if (isABaby(player)) {
+            event.setCancelled(true);
+        }
+    }
+
+
+    private boolean isABaby(Player player) {
+        // Player has Character?
+        PlayerRole role = RoleManager.get(player);
+        if (role == null)
+            return false;
+        // Character has phasemanager?
+        PhaseManager phaseManager = RoleManager.get(player).pm;
+        if (phaseManager == null)
+            return false;
+        // Phase is Baby?
+        if (phaseManager.getCurrentPhase().getName().equalsIgnoreCase("baby")) {
+            return true;
+        }
+        return false;
     }
 }
