8ae1036c8a6b25f53cf339ff4a8cb8ea70f23420
Stitchinger <g.silitsch@gmail.com>
2022-05-26 00:12:34 +0200

    - Join Message

--- src/main/java/io/georgeous/mcgenerations/listeners/PlayerJoinListener.java
+++ src/main/java/io/georgeous/mcgenerations/listeners/PlayerJoinListener.java
@@ -6,6 +6,7 @@ import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -19,20 +20,24 @@ public class PlayerJoinListener implements Listener {
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
 
+        welcomeMessage(player);
         playerManager.initPlayer(player);
         roleManager.initPlayer(player);
 
-        welcomeMessage(player);
         event.setJoinMessage("");
     }
 
     private void welcomeMessage(Player player){
-        player.sendMessage("Welcome to One Hour One Life");
-        BaseComponent[] component = new ComponentBuilder("Join the ")
+        player.sendMessage("§6Welcome to §l§cONE §fHOUR §cONE §fLIFE");
+        BaseComponent[] component = new ComponentBuilder("Please click here to join our ")
+                .color(ChatColor.GOLD)
                 .append("Discord!").color(ChatColor.BLUE).underlined(true)
                 .event(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://discord.gg/U262bxT4jh"))
+                .event(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
+                        new ComponentBuilder("Click to visit!").color(ChatColor.GRAY).italic(true).create()))
                 .create();
         player.spigot().sendMessage(component);
+        player.sendMessage("");
     }
 
 
